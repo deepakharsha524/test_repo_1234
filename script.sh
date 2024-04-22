554c5f8cf80c4e5f7d8d8f80c2013cef26ce3ad2
root <root@ip-172-31-53-10.ec2.internal>
2024-04-22 14:06:13 +0000

    Modified script.sh and created new file

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This is a script to learn shell scripting basics
-echo " this is harsha"
+
 var1=harsha
 var2=21
 
 echo $0
 
-
+echo "this is to validate functions"
 function addtion() {
   val=$(expr $1 + $2)
   echo add value is $val
